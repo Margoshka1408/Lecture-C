354fcc99124eeaa8b4f6cbab14dc2d4b977b1529
Margarita <margoshka_d@mail.ru>
2022-12-10 23:28:11 +0300

    Lesson 1

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username=Console.ReadLine();//ToLower позволяет писать любыми буквами
+if (username.ToLower()=="Маша")
+{
+    Console.Write("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
