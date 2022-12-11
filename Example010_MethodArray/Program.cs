530733e643fbb2a58c5cd8406c02f89212758f6d
Margarita <margoshka_d@mail.ru>
2022-12-11 11:18:55 +0300

    Lesson 2

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array={23,6,3,66,74,74,35};
+int n=array.Length;//количество элементов массива
+int find=74;
+int index=0;
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;//прерви
+    }
+    //index=index+1;
+    index++;
+}
